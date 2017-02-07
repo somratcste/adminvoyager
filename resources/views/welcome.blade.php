d05bc3a903e8026cbfd25856d4cca84b7aa891c9
somratcste <somratcste@gmail.com>
2017-02-07 11:18:43 +0600

    add adminvoyager package

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 5.3
                 </div>
 
                 <div class="links">
